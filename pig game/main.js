0854d2b905163a8e927efb97cf7d24ecc0e0e9d8
yomnamohamed2005 <yomnamohamed30000@gmail.com>
2025-12-12 02:21:19 +0200

    Fix formatting and enable player switching in the game logic

--- pig game/main.js
+++ pig game/main.js
@@ -43,14 +43,15 @@ document.querySelector('.btn--roll').addEventListener('click',function()
    }
 
  }
+
+
+})
  function switchPlayer(){
   document.querySelector('.player--0').classList.toggle('player--active');
 document.querySelector('.player--1').classList.toggle('player--active');
 activePlayer = activePlayer ===0?1 : 0;
 
  }
-
-})
 
 document.querySelector('.btn--hold').addEventListener('click',function(){
   if(activePlayer===0)
@@ -84,20 +85,20 @@ document.querySelector('.btn--hold').addEventListener('click',function(){
 })
 
 document.querySelector('.btn--new').addEventListener('click',function(){
-  let activePlayer=1;
-let score1 =0;
-let totalscore1= 0;
-let score2=0;
-let totalscore2=0;
- document.getElementById('score--0').textContent=totalscore1;
+   //activePlayer=1;
+ score1 =0;
+ totalscore1= 0;
+  score2=0;
+   totalscore2=0;
+  document.getElementById('score--0').textContent=totalscore1;
   document.getElementById('current--0').textContent=score1;
-   document.getElementById('current--1').textContent=score2;
-     document.getElementById('score--1').textContent=totalscore2;
+  document.getElementById('current--1').textContent=score2;
+  document.getElementById('score--1').textContent=totalscore2;
   document.querySelector('.pl2').style.color='blue';
-        document.querySelector('.btn--roll').disale= true; 
-           document.querySelector('.pl1').style.color='blue';
-            document.querySelector('.pl2').textContent="player2";
-             document.querySelector('.pl1').textContent="player1";
+  document.querySelector('.btn--roll').disaled= true; 
+  document.querySelector('.pl1').style.color='blue';
+  document.querySelector('.pl2').textContent="player2";
+  document.querySelector('.pl1').textContent="player1";
 
   switchPlayer();
 
